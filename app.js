95fd5dd6df6dde004dfb08223fcadd8ce1f7b044
Jeison Vera <jw.vera.r@gmail.com>
2024-10-09 19:16:26 -0500

    'add config prettier, eslint and format '

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-import express from "express";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+const app = express();
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extends: false }));
+app.use(morgan('dev'));
 
-const app=express();
-
-app.use(express.json())
-
-app.get("/", (req, res) => {
-    res.send("<h1>server</h1>");
-  });
-
-
+app.get('/', (req, res) => {
+  res.send('<h1>server</h1>');
+});
 
 export default app;
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import app from "./app.js";
-const PORT=3030
-app.listen(PORT)
+import app from './app.js';
+const port = process.env.PORT || 3000;
+
+const server = (port) => {
+  try {
+    app.listen(port);
+  } catch (error) {
+    console.log({ Error: error || 'error in server' });
+  }
+};
+
+server(port);
