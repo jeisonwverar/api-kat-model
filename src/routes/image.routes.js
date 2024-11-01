0af7ee070e2df8ba314c55335be21ebd637a4436
Jeison Vera <jw.vera.r@gmail.com>
2024-11-01 16:04:31 -0500

    auth required

--- src/routes/image.routes.js
+++ src/routes/image.routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import authRequired from '../middlewares/validateToken.js'
 import {
   createImage,
   getImage,
@@ -8,9 +9,9 @@ import {
 
 const routerImage = Router();
 
-routerImage.get('/image/:id?', getImage);
-routerImage.post('/image', createImage);
-routerImage.put('/image/:id', updateImage);
-routerImage.delete('/image/:id', deleteImage);
+routerImage.get('/image/:id?',authRequired ,getImage);
+routerImage.post('/image',authRequired ,createImage);
+routerImage.put('/image/:id',authRequired ,updateImage);
+routerImage.delete('/image/:id',authRequired ,deleteImage);
 
 export default routerImage;
