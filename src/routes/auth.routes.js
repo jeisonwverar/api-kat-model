cb7bccc13ff82cd175224853b1e02ede1d6e96ff
Jeison Vera <jw.vera.r@gmail.com>
2024-11-05 16:15:39 -0500

    login and register schema

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { register, login,logout } from '../controllers/auth.controller.js';
+import {validateSchema} from '../middlewares/validateSchema.js'
+import { registerSchema, loginSchema } from '../schema/auth.schema.js';
 const authRouter = Router();
 
-authRouter.post('/register', register);
-authRouter.post('/login', login);
+authRouter.post('/register',validateSchema(registerSchema), register);
+authRouter.post('/login',validateSchema(loginSchema), login);
 authRouter.post('/logout',logout);
 authRouter.get('/verify');
 
--- src/schema/auth.schema.js
+++ src/schema/auth.schema.js
@@ -1,1 +1,36 @@
 import z from 'zod';
+
+export const registerSchema=z.object({
+    name:z.string({
+        required_error:'name is required'
+    }).trim().min(2,{
+        message:'The name must be at least 2 characters'
+    }).max(100,{
+        message:'The name must have a maximum of 120 characters'
+    }),
+    password:z.string({
+        required_error:'Password is required'
+    }).min(5,{
+        message:'password must be at least 5 characters'
+    }).max(20,{
+        message:'The password must have a maximum of 100 characters'
+    }),
+    email:z.string({
+        required_error:'Email is required'
+    }).email({
+        message:'Invalid email'
+    })
+});
+
+export const loginSchema=z.object({
+    email:z.string({
+        required_error:'Email is required'
+    }).email({
+        message:'Invalid email'
+    }),
+    password:z.string({
+        required_error:'password is required'
+    }).min(5,{
+        message:'password must be at least 5 characters'
+    })
+});
