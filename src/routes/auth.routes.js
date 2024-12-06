332039e19dae8d6ce360faae61e1800493d76834
Jeison Vera <jw.vera.r@gmail.com>
2024-12-06 14:47:46 -0500

    configuration auth.route in validate

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { register, login, logout,validate } from '../controllers/auth.controller.js';
 import { validateSchema } from '../middlewares/validateSchema.js';
 import { registerSchema, loginSchema } from '../schema/auth.schema.js';
-import { validate } from 'uuid';
+
 const authRouter = Router();
 
 authRouter.post('/register', validateSchema(registerSchema), register);
