77124958c7eb155bcf9d2e63fc66ba6aed4e7588
Jeison Vera <jw.vera.r@gmail.com>
2024-12-19 11:38:33 -0500

    cambios en controller verificacion en local

--- src/controllers/transformer.controller.js
+++ src/controllers/transformer.controller.js
@@ -2,10 +2,11 @@ import generateTryOn from '../services/transformer.service.js';
 
 const transformer = async (req, res) => {
   try {
+    
     const { text = '', denoisingSteps = 30, seed = 42 } = req.body;
     const personImage = req.files['personImage']?.[0];
     const clothingImage = req.files['clothingImage']?.[0];
-
+    //console.log(personImage,clothingImage)
     //const personImageBlob = new Blob([personImage.buffer], { type: personImage.mimetype });
     //const clothingImageBlob = new Blob([clothingImage.buffer], { type: clothingImage.mimetype });
     if (!personImage || !clothingImage) {
