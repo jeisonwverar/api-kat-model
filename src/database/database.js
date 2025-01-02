37f465cd43a9f6241867e2aaa5260d41b0339b5c
Jeison Vera <jw.vera.r@gmail.com>
2025-01-02 16:45:02 -0500

    localhost

--- src/database/database.js
+++ src/database/database.js
@@ -1,23 +1,17 @@
 import { Sequelize } from 'sequelize';
 import environmentVariable from '../config/config.js';
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = environmentVariable;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME , DB_PORT_DB} = environmentVariable;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'postgres',
-  port: 5432,
-  dialectOptions: {
-    ssl: {
-      require: true, // Obliga el uso de SSL
-      rejectUnauthorized: false // Acepta certificados autofirmados
-    }
-  },
-  pool: {
+  port:  DB_PORT_DB,
+  /* pool: {
     max: 5,
     min: 0,
     acquire: 30000,
     idle: 10000
-  }
+  } */
 });
 
 export default sequelize;
